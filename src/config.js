df4cb004cbc5f56fbf271c523e9d9763b3043193
Kris Kettendorf <triggxl@hotmail.com>
2021-05-10 20:55:49 -0400

    heroku

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  API_URL: process.env.NODE_ENV === 'production' ? 'heroku url' : 'http://localhost:8002'
+  API_URL: process.env.NODE_ENV === 'production' ? 'https://ancient-chamber-86595.herokuapp.com/' : 'http://localhost:8002'
 }
