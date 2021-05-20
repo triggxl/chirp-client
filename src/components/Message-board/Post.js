6a98d8526d04ed5ebf40b642c6563769f03beeb7
Kris Kettendorf <triggxl@hotmail.com>
2021-05-20 17:15:07 -0400

    edit and delete functionalities work without error, are persisting as an empty comment upon reload; also not grabbing text when editing

--- src/components/Message-board/Post.js
+++ src/components/Message-board/Post.js
@@ -156,7 +156,7 @@ class Post extends React.Component {
     }
 
     const editedReply = post.replies.find(reply => reply.id === this.state.replyIdToBeEdited);
-    const editedText = this.state.title || (editedReply && editedReply.title)
+    const editedText = this.state.content || (editedReply && editedReply.content)
     return (
       <chirpContext.Consumer>
         {context => {
@@ -170,7 +170,7 @@ class Post extends React.Component {
                 <React.Fragment>
                   <tr key="cr-sec" id="cr-sec"> Content:</tr>
                   <tr key="sd">
-                    <td className="col-span" colSpan={6}>{post.title}
+                    <td className="col-span" colSpan={6}>{post.content}
                       <section key="section"></section>
                       {/* stateful logic to display textarea */}
                       {this.state.isReplying ?
